15b50958fc4bb873aa73a887cf5675e7756aaa53
spaceship1548 <spaceship1548@gmail.com>
2026-06-13 17:54:28 +0530

    add leetcode solution 28

---
+++ Index_first_string_occurance_28.py
@@ -0,0 +1,10 @@
+# Leet Code problem 28. Find the Index of the First Occurrence in a String
+# link:https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+# Jun 13, 2026
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle in haystack:
+            return haystack.index(needle)
+        else:
+            return -1
